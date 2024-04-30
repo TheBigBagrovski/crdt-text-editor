d7a1d6dbbe1912a79ba9874a2bd1bb7c71e28866
Matvey Bagrov <matvey.bagr77@mail.ru>
2024-04-30 11:40:59 +0300

    Рабочая тестовая версия, требуется ввод списка пиров на каждом клиенте, требуется добавление сигнального сервера

--- src/main/java/okp/nic/Main.java
+++ src/main/java/okp/nic/Main.java
@@ -1,7 +1,31 @@
 package okp.nic;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("================================");
+        System.out.print("Input host = ");
+        String host = in.nextLine();
+        System.out.print("Input port = ");
+        int port = in.nextInt();
+
+        System.out.println("--------------------------------");
+        List<String> peers = new ArrayList<>();
+        System.out.print("Enter number of peers = ");
+        int nPeers = in.nextInt();
+        String buf = in.nextLine(); // consume \n after nextInt
+        for (int i = 1; i <= nPeers; i++) {
+            System.out.print("> Peer " + i + " URI = ");
+            String peer = in.nextLine();
+            peers.add(peer);
+        }
+
+        Controller controller = new Controller(host, port, peers);
+        controller.start();
     }
 }
