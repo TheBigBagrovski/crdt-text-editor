b4d5c3b700c0842863ca7325c1a9e4f3203eb142
Matvey Bagrov <matvey.bagr77@mail.ru>
2024-05-08 20:36:51 +0300

    New crdt implementation

--- src/main/java/okp/nic/editor/TextEditor.java
+++ src/main/java/okp/nic/editor/TextEditor.java
@@ -192,9 +192,9 @@ public class TextEditor extends JFrame implements CaretListener, DocumentListene
     }
 
     public void pause() {
-        importDialog = new JDialog(this, "Импорт текста", true);
+        importDialog = new JDialog(this, getString("Импорт текста"), true);
         importDialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
-        JLabel messageLabel = new JLabel("Идет импорт текста...");
+        JLabel messageLabel = new JLabel(getString("Идет импорт текста..."));
         importDialog.add(messageLabel);
         importDialog.pack();
         importDialog.setLocationRelativeTo(this);
