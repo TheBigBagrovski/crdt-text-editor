3b9ba698ffe2bc777fcc82e4dcad16b9ba3c60ac
Matvey Bagrov <matvey.bagr77@mail.ru>
2024-05-11 04:27:54 +0300

    Полностью рабочие копирование, вырезание и вставка

--- src/main/java/okp/nic/gui/editor/TextEditor.java
+++ src/main/java/okp/nic/gui/editor/TextEditor.java
@@ -161,7 +161,7 @@ public class TextEditor extends JFrame implements CaretListener, DocumentListene
             @Override
             public void actionPerformed(ActionEvent e) {
                 copiedText = selectedText;
-                controller.onlyInDocLocalDeleteRange(selectStartPos, selectEndPos);
+                controller.cut(selectStartPos, selectEndPos);
             }
         });
 
