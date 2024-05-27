354fb2ee2f9933d68c742d3884b17378bcabe527
Matvey Bagrov <matvey.bagr77@mail.ru>
2024-05-27 03:02:42 +0300

    changes

--- src/main/java/okp/nic/network/messenger/SignalListener.java
+++ src/main/java/okp/nic/network/messenger/SignalListener.java
@@ -11,5 +11,4 @@ public interface SignalListener {
 
     void handleRemoteCurrentStateRequest(String peerAddress);
 
-
 }
--- src/main/java/okp/nic/presenter/Presenter.java
+++ src/main/java/okp/nic/presenter/Presenter.java
@@ -26,3 +26,4 @@ public interface Presenter {
     void loadFile();
 
 }
+
